a9e066d90dd8ba4f7d8e864d6fe6e45b4b44c4b7
another_danny <dave.ancor@gmail.com>
2026-03-15 14:14:16 +0100

    feat: added '--' to read, used in bash to indicate 100% string input. Fixes '$' and should fix other signs as well

--- mpv-yt-watch.sh
+++ mpv-yt-watch.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-### mpv-yt-watch.sh ###
+### mpv_yt_watch_v4.sh ###
 # - Added search string functionality, now you no longer need a browser at all really
 # - Need for url validation no longer required
 # - Removed unnecessary usage of caching to stdoutput and stderr
 # - Addition of 240p resolution option
 # - Change over from search variable grep to cache variable grep, allowing for all language support (korean, japanese etc.) [may require wqy-zenhei on arch linux]
-# - Replaced relevant echoes and reads with just reads, simplifying the code by a few lines
 
-read -p "Input search string <lowercase only>: " search
-echo "You are searching for: $search"
+echo "Input search string <lowercase only>: "
+read -- search
+echo $search
 if [[ -z "$search" ]]; then
   echo "No search string inputted"
 else
-  read -p "Would you like to change the resolution? (default is dependent on video) [Y/n]: " resConfirm
+  echo "You are searching for: $search"
+  echo "Would you like to change the resolution? (default is dependent on video) [Y/n]"
+  read resConfirm
   if [[ "$resConfirm" == [Yy] ]]; then
     echo -e "Available Resolutions: "
     resInput=$(yt-dlp ytsearch1:"$search" --list-formats | grep -e '240p\|480p\|720p\|1080p\|1440p\|2160p' | awk '{print $14;}' | tr -d 'p,' | tr -s '\n' | uniq | nl)
     echo "$resInput"
-    read -p "Choose resolution [1|2|3|4|5|6]: " resInput
+    echo "Choose resolution [1|2|3|4|5|6]: "
+    read resInput
     case $resInput in
     "1")
 	    cache=$(yt-dlp ytsearch1:"$search" | grep "Destination" | tail -n 1 | sed 's/\[download] Destination: //g' | sed 's/.\{23\}$//')
